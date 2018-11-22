ae07a2c5726b3bdc0a9ec2c5f530d60662a8d90e
caiosousa21 <caiofmsousa@gmail.com>
2018-11-22 12:56:42 -0200

    Gerenciando Saídas no branch teste

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,10 +1,17 @@
 import _ from 'lodash';
+import printMe from './print.js' 
 
 function component(){
     let element = document.createElement('div');
+    var btn = document.createElement('button');
 
     //função join do lodash, recebe dois parâmetros, um array de strings, e uma string para inserir entre eles
     element.innerHTML = _.join(['Hello', 'caio'], ' * ');
+
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
 
     return element;
 }
