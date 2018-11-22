83592c240f2be2739c2becbb7579a5191d41812d
caiosousa21 <caiofmsousa@gmail.com>
2018-11-22 10:55:24 -0200

    Gerenciador de Output

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,21 +1,10 @@
 import _ from 'lodash';
-import './style.css'
-import Icon from './icon.png';
-import Data from './data.xml'
 
 function component(){
     let element = document.createElement('div');
 
     //função join do lodash, recebe dois parâmetros, um array de strings, e uma string para inserir entre eles
     element.innerHTML = _.join(['Hello', 'caio'], ' * ');
-    //adicionando classe ao element
-    element.classList.add('hello');
-
-    var myIcon = new Image();
-    myIcon.src = Icon;
-    element.appendChild(myIcon);
-
-    console.log(Data);
 
     return element;
 }
