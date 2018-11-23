c019dfd4617c6f1b7aaf6ca4051d2e6dd95a38dc
caiosousa21 <caiofmsousa@gmail.com>
2018-11-23 11:05:16 -0200

    Estudando quebar o código em bundles, permitindo usa-lo apenas quando interessante ou paralelamente

--- webpack-demo/dist/another.bundle.js
+++ webpack-demo/dist/another.bundle.js
@@ -81,7 +81,7 @@
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = "./src/index.js");
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/outroModulo.js");
 /******/ })
 /************************************************************************/
 /******/ ({
@@ -119,15 +119,15 @@ eval("module.exports = function(module) {\n\tif (!module.webpackPolyfill) {\n\t\
 
 /***/ }),
 
-/***/ "./src/index.js":
-/*!**********************!*\
-  !*** ./src/index.js ***!
-  \**********************/
+/***/ "./src/outroModulo.js":
+/*!****************************!*\
+  !*** ./src/outroModulo.js ***!
+  \****************************/
 /*! no exports provided */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! lodash */ \"./node_modules/lodash/lodash.js\");\n/* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(lodash__WEBPACK_IMPORTED_MODULE_0__);\n\r\n\r\nfunction component() {\r\n    let element = document.createElement('div');\r\n    element.innerHTML = lodash__WEBPACK_IMPORTED_MODULE_0___default.a.join(['Hello', 'webpack'], ' ');\r\n\r\n    return element;\r\n}\r\n\r\ndocument.body.appendChild(component());\n\n//# sourceURL=webpack:///./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! lodash */ \"./node_modules/lodash/lodash.js\");\n/* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(lodash__WEBPACK_IMPORTED_MODULE_0__);\n\r\n\r\nconsole.log(\r\n  lodash__WEBPACK_IMPORTED_MODULE_0___default.a.join(['Another', 'module', 'loaded!'], ' ')\r\n);\n\n//# sourceURL=webpack:///./src/outroModulo.js?");
 
 /***/ })
 
--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,21 +1,8 @@
 import _ from 'lodash';
-import './style.css'
-import Icon from './icon.png';
-import Data from './data.xml'
 
-function component(){
+function component() {
     let element = document.createElement('div');
-
-    //função join do lodash, recebe dois parâmetros, um array de strings, e uma string para inserir entre eles
-    element.innerHTML = _.join(['Hello', 'caio'], ' * ');
-    //adicionando classe ao element
-    element.classList.add('hello');
-
-    var myIcon = new Image();
-    myIcon.src = Icon;
-    element.appendChild(myIcon);
-
-    console.log(Data);
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
     return element;
 }
