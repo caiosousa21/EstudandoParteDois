45bf693338cf14a65361bf3701872ae5aefafb6a
caiosousa21 <caiofmsousa@gmail.com>
2018-11-23 10:38:35 -0200

    Config separada em 3 módulos

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,22 +1,15 @@
-import _ from 'lodash';
-import './style.css'
-import Icon from './icon.png';
-import Data from './data.xml'
+import{cube}from './math.js' 
+
+if (process.env.NODE_ENV !== 'production'){
+    console.log('Estamos em modo de desenvolvimento');
+}
 
 function component(){
-    let element = document.createElement('div');
-
-    //função join do lodash, recebe dois parâmetros, um array de strings, e uma string para inserir entre eles
-    element.innerHTML = _.join(['Hello', 'caio','!'], ' * ');
-    //adicionando classe ao element
-    element.classList.add('hello');
-
-    var myIcon = new Image();
-    myIcon.src = Icon;
-    element.appendChild(myIcon);
-
-    console.log(Data);
-
+    let element = document.createElement('pre');
+    element.innerHTML=[
+        'Hello caio!',
+        '5 ao cubo é igual a '+cube(5)
+    ].join('\n\n')
     return element;
 }
 
